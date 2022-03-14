024c1816a938c8ebf149ef91ee09bc35d7ff5c7a
xlazure <56347002+xlazure@users.noreply.github.com>
2022-03-14 21:59:22 +0100

    add tailwind, styled-components, axios

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./public/**/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "375px",
+        md: "834px",
+        lg: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
