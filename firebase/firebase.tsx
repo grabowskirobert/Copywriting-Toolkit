b18aa9ea2b286f798b00f328e1484d6e6bd4dc48
Robert Grabowski <rrobertgrabowski@gmail.com>
2022-04-12 22:17:37 +0200

    removed env file

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -1,18 +1,18 @@
-import {getAuth} from 'firebase/auth'
-import { FirebaseApp, initializeApp } from "firebase/app";
-import {getFirestore} from '@firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { FirebaseApp, initializeApp } from 'firebase/app'
+import { getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
-};
+  apiKey: 'AIzaSyCNvkuajRJloP5c22SZ_O0lvBvqDlJARnE',
+  authDomain: 'copywriting-toolkit.firebaseapp.com',
+  projectId: 'copywriting-toolkit',
+  storageBucket: 'copywriting-toolkit.appspot.com',
+  messagingSenderId: '287829564531',
+  appId: '1:287829564531:web:0ee494167db4a4acf4de06',
+  measurementId: 'G-ZVG9V1VSG1',
+}
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
+const app: FirebaseApp = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
